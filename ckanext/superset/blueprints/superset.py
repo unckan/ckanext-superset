c4c42847afe1216a36b68f7abf658844f5c833cf
german <germankay@gmail.com>
2024-11-20 13:36:07 -0300

    flake

--- ckanext/superset/blueprints/superset.py
+++ ckanext/superset/blueprints/superset.py
@@ -7,13 +7,14 @@ superset = Blueprint(
     'superset_dashboard', __name__, url_prefix='/superset-dashboard'
 )
 
+
 @superset.route('/')
 @require_sysadmin_user
 def index():
     # Datos informativos
     superset_url = 'http://superset.ckan.local'
     datasets_count = stats_lib.Stats().get_num_packages_total()
-    databases_count = 5  # Cambiar por la lógica que calcule las bases de datos
+    databases_count = stats_lib.Stats().get_num_datasets()
 
     extra_vars = {
         'superset_url': superset_url,
