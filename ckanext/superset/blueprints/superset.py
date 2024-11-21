a194e5c0653ab938b4d190c6d22359b2d85d9446
avdata99 <andres@data99.com.ar>
2024-11-20 22:06:49 -0300

    Fix import

--- ckanext/superset/blueprints/superset.py
+++ ckanext/superset/blueprints/superset.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from ckan.plugins.toolkit import config, render
-from ckanext.api_tracking.decorators import require_sysadmin_user
+from ckanext.superset.decorators import require_sysadmin_user
 
 
 superset_bp = Blueprint('superset_blueprint', __name__, url_prefix='/apache-superset')
@@ -11,13 +11,12 @@ superset_bp = Blueprint('superset_blueprint', __name__, url_prefix='/apache-supe
 def index():
     # Datos informativos
     superset_url = config.get('ckanext.superset.instance.url')
-    datasets_count = config.get('ckanext.superset.datasets.count')
-    databases_count = config.get('ckanext.superset.databases.count')
+    datasets_count = 0
+    databases_count = 0
 
     extra_vars = {
         'superset_url': superset_url,
         'datasets_count': datasets_count,
         'databases_count': databases_count,
-        'active': 'superset-dashboard',
     }
     return render('superset/index.html', extra_vars)
