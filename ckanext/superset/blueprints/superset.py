15405377f060de8cc6da0b27b5f7a801910ba142
avdata99 <andres@data99.com.ar>
2024-11-21 23:13:18 -0300

    Preparing package create

--- ckanext/superset/blueprints/superset.py
+++ ckanext/superset/blueprints/superset.py
@@ -1,19 +1,24 @@
+import logging
 from flask import Blueprint
-from ckan.plugins.toolkit import config, render
+from ckan import model
+from ckan.common import current_user, request
+from ckan.plugins import toolkit as tk
 
 from ckanext.superset.config import get_config
 from ckanext.superset.decorators import require_sysadmin_user
 from ckanext.superset.data.main import SupersetCKAN
+from ckanext.superset.utils import slug
 
 
+log = logging.getLogger(__name__)
 superset_bp = Blueprint('superset_blueprint', __name__, url_prefix='/apache-superset')
 
 
-@superset_bp.route('/')
+@superset_bp.route('/', methods=['GET'])
 @require_sysadmin_user
 def index():
     # Datos informativos
-    superset_url = config.get('ckanext.superset.instance.url')
+    superset_url = tk.config.get('ckanext.superset.instance.url')
     cfg = get_config()
     sc = SupersetCKAN(**cfg)
     sc.load_datasets()
@@ -30,19 +35,46 @@ def index():
         'databases_count': databases_count,
         'databases': databases,
     }
-    return render('superset/index.html', extra_vars)
+    return tk.render('superset/index.html', extra_vars)
 
 
 @superset_bp.route('/create-dataset/<string:superset_dataset_id>')
 @require_sysadmin_user
 def create_dataset(superset_dataset_id):
     """ Create a new CKAN dataset from a Superset dataset """
-    # Check if the dataset exists in CKAN
-    cfg = get_config()
-    sc = SupersetCKAN(**cfg)
-    superset_dataset = sc.get_dataset(superset_dataset_id)
 
-    extra_vars = {
-        'superset_dataset': superset_dataset,
-    }
-    return render('superset/create-dataset.html', extra_vars)
+    if request.method == 'GET':
+        # Check if the dataset exists in CKAN
+        cfg = get_config()
+        sc = SupersetCKAN(**cfg)
+        superset_dataset = sc.get_dataset(superset_dataset_id)
+
+        extra_vars = {
+            'superset_dataset': superset_dataset,
+        }
+        return tk.render('superset/create-dataset.html', extra_vars)
+
+    if request.method == 'POST':
+        # Create the dataset
+        ckan_dataset_title = request.form.get('ckan_dataset_title')
+        # generate a slug name
+        ckan_dataset_name = slug(ckan_dataset_title)
+        # ensure the name is unique
+        c = 2
+        while pkg := model.Package.filter(name=ckan_dataset_name).first():
+            log.warning(f'Package name {ckan_dataset_name} already exists for package {pkg.id}')
+            ckan_dataset_name = f'{ckan_dataset_name}-{superset_dataset_id}-{c}'
+            c += 1
+
+        # Create the dataset
+        action = tk.get_action("package_create")
+        context = {'user': current_user.name}
+        data = {
+            'name': ckan_dataset_name,
+            'title': ckan_dataset_title,
+            'notes': request.form.get('ckan_dataset_notes'),
+            'owner_org': request.form.get('ckan_dataset_owner_org'),
+            'private': request.form.get('ckan_dataset_private'),
+            'superset_dataset_id': superset_dataset_id,
+        }
+        pkg = action(context, data)
