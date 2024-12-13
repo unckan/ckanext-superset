bdacfd966237a4878e5e50477f9e7275b9dcb0c0
german <germankay@gmail.com>
2024-12-13 11:14:04 -0300

    corrigiendo migrate

--- ckanext/superset/tests/fixtures.py
+++ ckanext/superset/tests/fixtures.py
@@ -4,5 +4,5 @@ import pytest
 @pytest.fixture
 def superset_migrate(migrate_db_for):
     # Migrar la base de datos de Superset
-    migrate_db_for('superset')
-    return True
+    # migrate_db_for('superset')
+    pass
