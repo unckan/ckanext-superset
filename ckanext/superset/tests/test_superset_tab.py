419bc2ef6987926843e55ba7ef371dcf6f7656ce
german <germankay@gmail.com>
2024-12-17 12:21:18 -0300

    Actualiza pruebas para Superset: modifica helpers.py y test_superset_tab.py, reemplaza test_chart.json con chart_32.json

--- ckanext/superset/tests/test_superset_tab.py
+++ ckanext/superset/tests/test_superset_tab.py
@@ -21,6 +21,7 @@ class TestSupersetViews:
     """Tests para las vistas de Superset"""
 
     def test_index_sysadmin_can_access(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un sysadmin puede acceder a la vista de Superset"""
         auth = {"Authorization": setup_data.sysadmin['token']}
         url = url_for('superset_blueprint.index')
         response = app_httpx_mocked.get(url, extra_environ=auth)
@@ -28,18 +29,20 @@ class TestSupersetViews:
         assert 'Superset charts' in response.body
 
     def test_index_non_sysadmin_cannot_access(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un usuario no sysadmin no puede acceder a la vista de Superset"""
         auth = {"Authorization": setup_data.user_regular['token']}
         url = url_for('superset_blueprint.index')
         response = app_httpx_mocked.get(url, extra_environ=auth, expect_errors=True)
         assert response.status_code == 403
 
     def test_create_dataset_sysadmin_can_create(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un sysadmin puede crear un dataset"""
         # Configuración de autenticación
         auth = {"Authorization": setup_data.sysadmin['token']}
         assert auth["Authorization"], "El token de autenticación está vacío o no válido"
 
         # Verificar acceso inicial al endpoint
-        chart_id = 'test_chart'
+        chart_id = 32
         url = url_for('superset_blueprint.create_dataset', chart_id=chart_id)
         response = app_httpx_mocked.get(url, extra_environ=auth)
         assert response.status_code == 200, "El endpoint no respondió correctamente"
@@ -71,6 +74,7 @@ class TestSupersetViews:
         assert 'Test Dataset' in dataset_response.body, "El título del dataset no está presente en la respuesta."
 
     def test_create_dataset_non_sysadmin_cannot_create(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un usuario no sysadmin no puede crear un dataset"""
         auth = {"Authorization": setup_data.user_regular['token']}
         chart_id = 'test_chart'
         url = url_for('superset_blueprint.create_dataset', chart_id=chart_id)
@@ -78,10 +82,12 @@ class TestSupersetViews:
         assert response.status_code == 403
 
     def test_update_dataset_sysadmin_can_update(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un sysadmin puede actualizar un dataset"""
         auth = {"Authorization": setup_data.sysadmin['token']}
+        assert auth["Authorization"], "El token de autenticación está vacío o no válido"
         # Crear un dataset con un recurso asociado al chart_id
         # Verificar acceso inicial al endpoint
-        chart_id = 'test_chart'
+        chart_id = '32'
         url = url_for('superset_blueprint.create_dataset', chart_id=chart_id)
         response = app_httpx_mocked.get(url, extra_environ=auth)
         assert response.status_code == 200, "El endpoint no respondió correctamente"
@@ -111,37 +117,28 @@ class TestSupersetViews:
 
         # Actualizar el dataset creado
         update_url = url_for('superset_blueprint.update_dataset', chart_id=chart_id)
-        update_data = {
-            'ckan_dataset_title': 'Test Dataset Updated',
-            'ckan_dataset_notes': 'Some notes updated',
-            'ckan_organization_id': setup_data.organization['id'],
-            'ckan_dataset_private': False,
-        }
-        update_response = app_httpx_mocked.post(update_url, extra_environ=auth, data=update_data)
+        update_response = app_httpx_mocked.post(update_url, extra_environ=auth)
         assert update_response.status_code == 200, f"Se esperaba un 200, pero se recibió {update_response.status_code}"
-        assert 'Dataset updated successfully.' in update_response.body, "El mensaje de éxito no está presente en la respuesta."
-
-        # Validar que el dataset actualizado se puede consultar en la lista de datasets
-        dataset_url = url_for('dataset.read', id='test-dataset')
-        dataset_response = app_httpx_mocked.get(dataset_url, extra_environ=auth)
-        assert dataset_response.status_code == 200, "El dataset creado no está disponible."
-        assert 'Test Dataset Updated' in dataset_response.body, "El título del dataset no está presente en la respuesta."
+        assert 'updated successfully.' in update_response.body, "El mensaje de éxito no está presente en la respuesta."
 
     def test_update_dataset_non_sysadmin_cannot_update(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un usuario no sysadmin no puede actualizar un dataset"""
         auth = {"Authorization": setup_data.user_regular['token']}
         chart_id = 'test_chart'
         url = url_for('superset_blueprint.update_dataset', chart_id=chart_id)
         response = app_httpx_mocked.post(url, extra_environ=auth, expect_errors=True)
         assert response.status_code == 403
 
-    # def test_list_databases_sysadmin_can_access(self, app_httpx_mocked, setup_data):
-    #     auth = {"Authorization": setup_data.sysadmin['token']}
-    #     url = url_for('superset_blueprint.list_databases')
-    #     response = app_httpx_mocked.get(url, extra_environ=auth)
-    #     assert response.status_code == 200
-    #     assert 'databases' in response.body
+    def test_list_databases_sysadmin_can_access(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un sysadmin puede acceder a la lista de bases de datos"""
+        auth = {"Authorization": setup_data.sysadmin['token']}
+        url = url_for('superset_blueprint.list_databases')
+        response = app_httpx_mocked.get(url, extra_environ=auth)
+        assert response.status_code == 200
+        assert 'databases' in response.body
 
     def test_list_databases_non_sysadmin_cannot_access(self, app_httpx_mocked, setup_data):
+        """Test para verificar que un usuario no sysadmin no puede acceder a la lista de bases de datos"""
         auth = {"Authorization": setup_data.user_regular['token']}
         url = url_for('superset_blueprint.list_databases')
         response = app_httpx_mocked.get(url, extra_environ=auth, expect_errors=True)
